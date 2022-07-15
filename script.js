5b7b334fe677c1a1b6eee8d4b6490b45af62b438
Kolapo Precious Akinrinlola <akinspreciousola@gmail.com>
2022-07-15 20:23:11 +0100

    create click and mouseover features for each div

--- script.js
+++ script.js
@@ -3,14 +3,32 @@ let gridContainer = document.getElementById('grid__container')
 let col = 16
 while (col > 0) {
   let newColDiv = document.createElement('div')
-  newColDiv.id = 'col__div'
+  newColDiv.className = 'col__div'
   gridContainer.appendChild(newColDiv)
   let row = 16
   while (row > 0) {
     let newRowDiv = document.createElement('div')
-    newRowDiv.id = 'row__div'
+    newRowDiv.className = 'row__div'
     newColDiv.appendChild(newRowDiv)
     row--
   }
   col--
+}
+
+let allRowElements = document.getElementsByClassName('row__div')
+
+let mouseClick = false
+window.onmousedown = function (e) {
+  if (e.button === 0) {
+    mouseClick = true
+  }
+}
+window.onmouseup = function () {
+  mouseClick = false
+}
+
+for (var i = 0; i < allRowElements.length; i++) {
+  allRowElements[i].onmouseover = () => {
+    if (mouseClick) console.log('hallo')
+  }
 }
