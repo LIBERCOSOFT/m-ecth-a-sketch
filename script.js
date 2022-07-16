48f43cba75bb5cd563ea86c6d60030491c204eed
Kolapo Precious Akinrinlola <akinspreciousola@gmail.com>
2022-07-16 16:33:50 +0100

    create simple menu elements and edit grid container to fit the specified amount of grids

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let gridContainer = document.getElementById('grid__container')
 
-let col = 16
+let givenRow = 17
+let col = givenRow
 while (col > 0) {
   let newColDiv = document.createElement('div')
   newColDiv.className = 'col__div'
   gridContainer.appendChild(newColDiv)
-  let row = 16
+  let row = givenRow
   while (row > 0) {
     let newRowDiv = document.createElement('div')
     newRowDiv.className = 'row__div'
+    newRowDiv.style.height = `${400 / givenRow}px`
+    newRowDiv.style.width = `${400 / givenRow}px`
     newColDiv.appendChild(newRowDiv)
     row--
   }
   col--
+}
+
+let colorVal = () => {
+  console.log(document.getElementById('color').value)
 }
 
 let allRowElements = document.getElementsByClassName('row__div')
