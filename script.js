4eb65fca91463927950bf5327326c14ee62f28c0
Kolapo Precious Akinrinlola <akinspreciousola@gmail.com>
2022-07-16 17:38:53 +0100

    implement the range slider with the grids to display amount of grid accrodint to slider value

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let gridContainer = document.getElementById('grid__container')
+let givenRC = 16
 
-let givenRow = 17
-let col = givenRow
-while (col > 0) {
-  let newColDiv = document.createElement('div')
-  newColDiv.className = 'col__div'
-  gridContainer.appendChild(newColDiv)
-  let row = givenRow
-  while (row > 0) {
-    let newRowDiv = document.createElement('div')
-    newRowDiv.className = 'row__div'
-    newRowDiv.style.height = `${400 / givenRow}px`
-    newRowDiv.style.width = `${400 / givenRow}px`
-    newColDiv.appendChild(newRowDiv)
-    row--
+let createGrid = (gridNum) => {
+  let col = gridNum
+  while (col > 0) {
+    let newColDiv = document.createElement('div')
+    newColDiv.className = 'col__div'
+    gridContainer.appendChild(newColDiv)
+    let row = gridNum
+    while (row > 0) {
+      let newRowDiv = document.createElement('div')
+      newRowDiv.className = 'row__div'
+      newRowDiv.style.height = `${400 / gridNum}px`
+      newRowDiv.style.width = `${400 / gridNum}px`
+      newColDiv.appendChild(newRowDiv)
+      row--
+    }
+    col--
   }
-  col--
 }
 
-let colorVal = () => {
-  console.log(document.getElementById('color').value)
+createGrid(givenRC)
+
+let gridInput = () => {
+  givenRC = document.getElementById('grid__value').value
+  gridContainer.innerHTML = ''
+  createGrid(givenRC)
+}
+
+let colorInput = () => {
+  console.log(document.getElementById('color__value').value)
 }
 
 let allRowElements = document.getElementsByClassName('row__div')
