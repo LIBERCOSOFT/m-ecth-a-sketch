d1ba11839b4c3c35c2a2b78ea95880c2559ac2d6
Kolapo Precious Akinrinlola <akinspreciousola@gmail.com>
2022-07-23 21:08:07 +0100

    implement all button functionalities except rainbow mode and implement the hover and click to change color on grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let gridContainer = document.getElementById('grid__container')
 let givenRC = 16
+let givenColor = document.getElementById('color__value').value
 
 let createGrid = (gridNum) => {
   let col = gridNum
+  let colIdentifier = 1
   while (col > 0) {
     let newColDiv = document.createElement('div')
     newColDiv.className = 'col__div'
@@ -11,12 +13,34 @@ let createGrid = (gridNum) => {
     while (row > 0) {
       let newRowDiv = document.createElement('div')
       newRowDiv.className = 'row__div'
+      newRowDiv.id = `cell${colIdentifier}`
       newRowDiv.style.height = `${400 / gridNum}px`
       newRowDiv.style.width = `${400 / gridNum}px`
       newColDiv.appendChild(newRowDiv)
       row--
+      colIdentifier++
     }
     col--
+  }
+  let allRowElements = document.getElementsByClassName('row__div')
+  let mouseClick = false
+  window.onmousedown = function (e) {
+    if (e.button === 0) {
+      mouseClick = true
+    }
+  }
+  window.onmouseup = function () {
+    mouseClick = false
+  }
+  for (var i = 0; i < allRowElements.length; i++) {
+    let id = allRowElements[i].id
+    let targetElement = document.getElementById(id)
+    targetElement.addEventListener('mouseover', function () {
+      if (mouseClick) targetElement.style.backgroundColor = givenColor
+    })
+    targetElement.addEventListener('mousedown', function () {
+      targetElement.style.backgroundColor = givenColor
+    })
   }
 }
 
@@ -28,24 +52,17 @@ let gridInput = () => {
   createGrid(givenRC)
 }
 
-let colorInput = () => {
-  console.log(document.getElementById('color__value').value)
+let colorMode = () => {
+  givenColor = document.getElementById('color__value').value
 }
 
-let allRowElements = document.getElementsByClassName('row__div')
-
-let mouseClick = false
-window.onmousedown = function (e) {
-  if (e.button === 0) {
-    mouseClick = true
-  }
-}
-window.onmouseup = function () {
-  mouseClick = false
+let eraseInput = () => {
+  givenColor = '#fff'
 }
 
-for (var i = 0; i < allRowElements.length; i++) {
-  allRowElements[i].onmouseover = () => {
-    if (mouseClick) console.log('hallo')
+let clearInput = () => {
+  let allRowElements = document.getElementsByClassName('row__div')
+  for (var i = 0; i < allRowElements.length; i++) {
+    allRowElements[i].style.backgroundColor = '#fff'
   }
 }
